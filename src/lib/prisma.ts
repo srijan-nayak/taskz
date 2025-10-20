db2d35a543923542547f346566fafec26b4b58d0
Srijan Nayak <srijannayak0@gmail.com>
2025-10-20 13:10:47 +0000

    Configure prisma for prod

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from "@/generated/prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma =
+  globalForPrisma.prisma ||
+  (process.env.NODE_ENV !== "production"
+    ? new PrismaClient()
+    : new PrismaClient().$extends(withAccelerate()));
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
